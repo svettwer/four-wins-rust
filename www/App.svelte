bdb59385b9194916bd8bd7a56ba5688f6785f4e2
Sven Hettwer <sven.hettwer@consol.de>
2021-03-16 09:12:21 +0100

    create game ui + greet on click

--- www/App.svelte
+++ www/App.svelte
@@ -1,21 +1,45 @@
 <script>
     import * as wasm from "four-wins"
-	export let name;
 
-    wasm.greet()
+    const stackConfig = {
+        defaultBackground: "bg-gray-100",
+        playerOne: "bg-red-800",
+        playerTwo: "bg-wellow-800"
+    }
+
+    let stacks = [
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0]
+    ]
+
+    
 </script>
 
 <main>
     <div class="pt-20">
-        <div class="p-3 md:container md:mx-auto md:w-2/6 bg-blue-500 object-center border-8 h-auto grid grid-cols-7 gap-4">
-            <div>
-                <div id="1x6" class="bg-gray-100 rounded-full h-16 w-16 flex items-center justify-center"></div>
-                <div id="1x5" class="bg-gray-100 rounded-full h-16 w-16 flex items-center justify-center"></div>
-                <div id="1x4" class="bg-gray-100 rounded-full h-16 w-16 flex items-center justify-center"></div>
-                <div id="1x3" class="bg-gray-100 rounded-full h-16 w-16 flex items-center justify-center"></div>
-                <div id="1x2" class="bg-gray-100 rounded-full h-16 w-16 flex items-center justify-center"></div>
-                <div id="1x1" class="bg-gray-100 rounded-full h-16 w-16 flex items-center justify-center"></div>
-            </div>
+        <div class="md:p-3 
+                    md:container 
+                    md:mx-auto
+                    md:w-2/6 
+                    md:h-auto 
+                    bg-blue-500 
+                    object-center 
+                    border-8 
+                    grid 
+                    grid-cols-{stacks.length} 
+                    gap-4">
+            {#each stacks as stack, i}
+                <div class="md:w-16" id="stack-{i}" on:click={wasm.greet}>
+                {#each stack as fieldCss}
+                    <div class="{stackConfig.defaultBackground} rounded-full md:h-16 md:w-16 flex items-center justify-center"></div>
+                {/each}
+                </div>
+            {/each}
         </div>
     </div>
 </main>
