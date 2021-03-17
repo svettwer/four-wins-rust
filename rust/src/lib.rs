843814f87ceee03b18f261dc23bb2105da3413ee
Sven Hettwer <sven.hettwer@consol.de>
2021-03-17 15:20:38 +0100

    added game logic and basi connection to frontend

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,6 +2,60 @@ mod utils;
 
 use wasm_bindgen::prelude::*;
 
+#[wasm_bindgen]
+pub struct FourWins {
+    layout: [[i8; 6]; 7],
+    pub current_player: i8
+}
+
+#[wasm_bindgen]
+impl FourWins {
+    pub fn new() -> FourWins {
+        FourWins { 
+            layout: [
+                [0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0]
+            ],
+            current_player: 1
+        }
+    }
+
+    pub fn player_action(&mut self, stack: usize){
+        if self.layout[stack][0] == 0{
+            for index in 0..self.layout[stack].len(){
+                let look_ahead_index = index + 1;
+                if look_ahead_index < self.layout[stack].len() {
+                   if self.layout[stack][look_ahead_index] != 0 {
+                    self.layout[stack][index] = self.current_player;
+                    break;
+                   }
+                } else{
+                    self.layout[stack][index] = self.current_player;
+                    break;
+                }
+            }
+            if self.current_player == 1 {
+                self.current_player = 2
+            } else{
+                self.current_player = 1
+            }
+        }
+    }
+
+    pub fn get_stack(&self, id: usize) -> js_sys::Int8Array  {
+        js_sys::Int8Array::from(&self.layout[id][..])
+    }
+
+    pub fn get_stack_count(&self) -> usize {
+        self.layout.len()
+    }
+}
+
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
 #[cfg(feature = "wee_alloc")]
--- www/App.svelte
+++ www/App.svelte
@@ -7,17 +7,17 @@
         playerTwo: "bg-wellow-800"
     }
 
-    let stacks = [
-        [0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0]
-    ]
+    let game = wasm.FourWins.new();
+    let rerender = 0    
 
-    
+    function handleClick(stackIndex){
+        console.log(`Stack index: ${stackIndex}`); 
+        console.log(`Current stack: ${game.get_stack(stackIndex)}`);
+        game.player_action(stackIndex);
+        rerender = rerender+1
+        console.log(rerender)
+        console.log(`Stack after player action: ${game.get_stack(stackIndex)}`);
+    }
 </script>
 
 <main>
@@ -31,12 +31,18 @@
                     object-center 
                     border-8 
                     grid 
-                    grid-cols-{stacks.length} 
+                    grid-cols-7 
                     gap-4">
-            {#each stacks as stack, i}
-                <div class="md:w-16" id="stack-{i}" on:click={wasm.greet}>
-                {#each stack as fieldCss}
-                    <div class="{stackConfig.defaultBackground} rounded-full md:h-16 md:w-16 flex items-center justify-center"></div>
+            {#each Array(game.get_stack_count() - 1) as _ , i}
+                <div class="md:w-16" id="stack-{i}" on:click={() => handleClick(i) }>
+                {#each game.get_stack(i) as value, j}
+                    {#if value === 1}
+                        <div rerender="{rerender}" val="{value}" class="{stackConfig.playerOne} rounded-full md:h-16 md:w-16 flex items-center justify-center"></div>
+                    {:else if value === 2}
+                        <div rerender="{rerender}" val="{value}" class="{stackConfig.playerTwo} rounded-full md:h-16 md:w-16 flex items-center justify-center"></div>
+                    {:else}
+                        <div rerender="{rerender}" val="{value}" class="{stackConfig.defaultBackground} rounded-full md:h-16 md:w-16 flex items-center justify-center"></div>
+                    {/if}
                 {/each}
                 </div>
             {/each}
