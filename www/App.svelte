2edc8fc6ecb8a89fc422f0118fc2747d6a07a1fe
Sven Hettwer <sven.hettwer@consol.de>
2021-03-19 13:53:05 +0100

    Show new game button, only if there is a winner

--- www/App.svelte
+++ www/App.svelte
@@ -108,7 +108,7 @@
                 {/each}
             {/if}
         </div>
-        {#if !winner}
+        {#if winner}
             <div class="text-center space-y-4">
                 <div class="text-2xl">Player <strong>{winner}</strong> won!</div>
                 <button class="font-bold text-lg rounded bg-blue-300 px-4 py-2 hover:bg-blue-500 transition-colors duration-200 hover:text-white" 
