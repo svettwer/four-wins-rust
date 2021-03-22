18b6ff1560e603fa7d06ce1245070809fd457e6f
Sven Hettwer <sven.hettwer@consol.de>
2021-03-22 17:30:27 +0100

    [WIP] choose game modes

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,23 +1,25 @@
 <script>
     import "tailwindcss/tailwind.css"
     import FourWins from "./FourWins.svelte";
+    import SetupGame from "./SetupGame.svelte";
 
-    let gameMode;
+    let gameMode = "singleplayer" //toggle to deactivate game mode dialog
+
+    function setGameMode(newGameMode){
+        gameMode = newGameMode
+    }
+
 </script>
 
 <main>
-    {#if gameMode}
+    {#if gameMode === "singleplayer"}
         <FourWins />
     {:else}
         <div class="p-8 space-y-8 bg-blue-500 h-screen">
             <div class="text-center">
                 <div class="text-4xl text-white">Four Wins</div>
             </div>
-            <div class="container mx-auto flex flex-col items-center justify-center space-y-4 text-white">
-                <div class="text-2xl">Select Game Mode</div>
-                <button class="text-xl font-bold text-lg rounded bg-blue-800 px-4 py-2 hover:bg-blue-300 transition-colors duration-200 hover:text-black">Singleplayer</button>
-                <button class="text-xl font-bold text-lg rounded bg-blue-800 px-4 py-2 hover:bg-blue-300 transition-colors duration-200 hover:text-black">Multiplayer</button>
-            </div>
+            <SetupGame onSetGameMode="{setGameMode}"/>
         </div>
     {/if}
     
---
+++ client/src/SetupGame.svelte
@@ -0,0 +1,34 @@
+<script>
+    export let onSetGameMode;
+
+    let buttonStyle = `
+            text-xl 
+            font-bold 
+            text-lg 
+            rounded 
+            bg-blue-800 
+            px-4 
+            py-2 
+            hover:bg-blue-300 
+            transition-colors 
+            duration-200 
+            hover:text-black
+            `
+    let gameMode
+
+    function dispatchGameModes(gameMode) {
+        if(gameMode === "singleplayer"){
+            onSetGameMode("singleplayer")
+        }
+    }
+</script>
+
+{#if gameMode === "multiplayer"}
+<div></div>
+{:else}
+    <div class="container mx-auto flex flex-col items-center justify-center space-y-4 text-white">
+        <div class="text-2xl">Select Game Mode</div>
+        <button class="{buttonStyle}" on:click={() => dispatchGameModes("singleplayer")}>Singleplayer</button>
+        <button class="{buttonStyle}" on:click={() => dispatchGameModes("multiplayer")}>Multiplayer</button>
+    </div>
+{/if}
