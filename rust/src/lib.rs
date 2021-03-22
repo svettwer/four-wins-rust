32c48b06b0918f98e0dc38e2c5cd2b8391cad06b
Sven Hettwer <sven.hettwer@consol.de>
2021-03-22 14:17:25 +0100

    refactored lib.rs

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -3,7 +3,6 @@ mod utils;
 use std::{usize};
 
 use wasm_bindgen::prelude::*;
-use js_sys;
 use serde::{Deserialize, Serialize};
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
@@ -29,6 +28,10 @@ impl WinConstellation {
     }
 }
 
+struct GameResult{
+    player: i8,
+    field_count: i8
+}
 
 #[wasm_bindgen]
 pub struct FourWins {
@@ -57,23 +60,8 @@ impl FourWins {
 
     pub fn player_action(&mut self, stack: usize){
         if self.layout[stack][0] == 0{
-            for index in 0..self.layout[stack].len(){
-                let look_ahead_index = index + 1;
-                if look_ahead_index < self.layout[stack].len() {
-                   if self.layout[stack][look_ahead_index] != 0 {
-                    self.layout[stack][index] = self.current_player;
-                    break;
-                   }
-                } else{
-                    self.layout[stack][index] = self.current_player;
-                    break;
-                }
-            }
-            if self.current_player == 1 {
-                self.current_player = 2
-            } else{
-                self.current_player = 1
-            }
+            self.add_coin_to_stack(stack);
+            self.switch_player();
         }
     }
 
@@ -82,119 +70,187 @@ impl FourWins {
     }
 
     pub fn get_winner(&mut self) -> Option<i8> {
-        struct GameResult{
-            player: i8,
-            field_count: i8
+
+        if let Some(winner) = self.check_columns() {
+            return winner;
         }
 
-        //check cols
-        for (stack_index, stack) in self.layout.iter().enumerate() {
-            let mut game_result = GameResult{player: 0, field_count: 0};
-            for (field_index, field) in stack.iter().enumerate() {
-                if game_result.player == *field {
-                    game_result.field_count += 1;
-                }else{
-                    game_result.player = *field;
-                    game_result.field_count = 1;
-                }
-
-                if game_result.player != 0 && game_result.field_count >= 4  {
-                    self.win_constellation = Some(WinConstellation{
-                        one: (stack_index, field_index-3),
-                        two: (stack_index, field_index-2),
-                        three: (stack_index, field_index-1),
-                        four: (stack_index, field_index),
-                    });
-                    return Some(game_result.player);
-                }
-            }
+        if let Some(winner) = self.check_rows() {
+            return winner;
         }
 
-        //check rows
-        for row in 0..6 {
+        if let Some(winner) = self.check_diagonal() {
+            return winner;
+        }
+
+        None
+    }
+
+    pub fn get_stack(&self, id: usize) -> js_sys::Int8Array  {
+        js_sys::Int8Array::from(&self.layout[id][..])
+    }
+
+    pub fn get_stack_count(&self) -> usize {
+        self.layout.len()
+    }
+
+
+    fn check_rows(&mut self) -> Option<Option<i8>> {
+        for row in 0..self.layout[0].len() {
             let mut game_result = GameResult{player: 0, field_count: 0};
-            for col in 0..7 {
+            for col in 0..self.layout.len() {
                 let field = self.layout[col][row];
-                if game_result.player == field {
-                    game_result.field_count += 1;
-                }else{
-                    game_result.player = field;
-                    game_result.field_count = 1;
-                }
+                compute_score(&mut game_result, &field);
 
-                if game_result.player != 0 && game_result.field_count >= 4  {
+                if game_won(&game_result)  {
                     self.win_constellation = Some(WinConstellation{
                         one: (col-3, row),
                         two: (col-2, row),
                         three: (col-1, row),
                         four: (col, row),
                     });
-                    return Some(game_result.player);
+                    return Some(Some(game_result.player));
                 }
             }
         }
+        None
+    }
 
-        //check diagnal
-        for col in 0..7 {
-            for row in 0..6 {
-                if col >= 3 && row + 3 < 6 {
-                    let mut game_result = GameResult{player: 0, field_count: 0};
-                    //to left case
-                    game_result.player = self.layout[col][row];
-                    game_result.field_count = 1;
-                    if self.layout[col-1][row+1] == game_result.player {
-                        game_result.field_count += 1;
-                    }
-                    if self.layout[col-2][row+2] == game_result.player {
-                        game_result.field_count += 1;
-                    }
-                    if self.layout[col-3][row+3] == game_result.player {
-                        game_result.field_count += 1;
-                    }
+    fn check_columns(&mut self) -> Option<Option<i8>> {
+        for (stack_index, stack) in self.layout.iter().enumerate() {
+            let mut game_result = GameResult{player: 0, field_count: 0};
+            for (field_index, field) in stack.iter().enumerate() {
+                compute_score(&mut game_result, field);
 
-                    if game_result.player != 0 && game_result.field_count >= 4  {
-                        self.win_constellation = Some(WinConstellation{
-                            one: (col, row),
-                            two: (col-1, row+1),
-                            three: (col-2, row+2),
-                            four: (col-3, row+3),
-                        });
-                        return Some(game_result.player);
-                    }
-                }
-
-                if col + 3 < 7 && row + 3 < 6 {
-                    let mut game_result = GameResult{player: 0, field_count: 0};
-                    //to right case
-                    game_result.player = self.layout[col][row];
-                    game_result.field_count = 1;
-                    if self.layout[col+1][row+1] == game_result.player {
-                        game_result.field_count += 1;
-                    }
-                    if self.layout[col+2][row+2] == game_result.player {
-                        game_result.field_count += 1;
-                    }
-                    if self.layout[col+3][row+3] == game_result.player {
-                        game_result.field_count += 1;
-                    }
-
-                    if game_result.player != 0 && game_result.field_count >= 4  {
-                        self.win_constellation = Some(WinConstellation{
-                            one: (col, row),
-                            two: (col+1, row+1),
-                            three: (col+2, row+2),
-                            four: (col+3, row+3),
-                        });
-                        return Some(game_result.player);
-                    }
+                if game_won(&game_result)  {
+                    self.win_constellation = Some(
+                        WinConstellation{
+                            one: (stack_index, field_index-3),
+                            two: (stack_index, field_index-2),
+                            three: (stack_index, field_index-1),
+                            four: (stack_index, field_index),
+                    });
+                    return Some(Some(game_result.player));
                 }
             }
         }
+        None
+    }
 
+    fn check_diagonal(&mut self) -> Option<Option<i8>> {
+        for col in 0..self.layout.len() {
+            for row in 0..self.layout[0].len() {
+                if let Some(value) = self.check_ascending(col, row) {
+                    return value;
+                }
+
+                if let Some(value) = self.check_decending(col, row) {
+                    return value;
+                }
+            }
+        }
+        None
+    }
+
+    fn check_decending(&mut self, col: usize, row: usize) -> Option<Option<Option<i8>>> {
+        if col + 3 < 7 && row + 3 < 6 {
+            let mut game_result = GameResult{player: 0, field_count: 0};
+            game_result.player = self.layout[col][row];
+            game_result.field_count = 1;
+            if self.layout[col+1][row+1] == game_result.player {
+                game_result.field_count += 1;
+            }
+            if self.layout[col+2][row+2] == game_result.player {
+                game_result.field_count += 1;
+            }
+            if self.layout[col+3][row+3] == game_result.player {
+                game_result.field_count += 1;
+            }
+
+            if game_result.player != 0 && game_result.field_count >= 4  {
+                self.win_constellation = Some(WinConstellation{
+                    one: (col, row),
+                    two: (col+1, row+1),
+                    three: (col+2, row+2),
+                    four: (col+3, row+3),
+                });
+                return Some(Some(Some(game_result.player)));
+            }
+        }
+        None
+    }
+
+    fn check_ascending(&mut self, col: usize, row: usize) -> Option<Option<Option<i8>>> {
+        if col >= 3 && row + 3 < 6 {
+            let mut game_result = GameResult{player: 0, field_count: 0};
+            //to left case
+            game_result.player = self.layout[col][row];
+            game_result.field_count = 1;
+            if self.layout[col-1][row+1] == game_result.player {
+                game_result.field_count += 1;
+            }
+            if self.layout[col-2][row+2] == game_result.player {
+                game_result.field_count += 1;
+            }
+            if self.layout[col-3][row+3] == game_result.player {
+                game_result.field_count += 1;
+            }
+
+            if game_result.player != 0 && game_result.field_count >= 4  {
+                self.win_constellation = Some(WinConstellation{
+                    one: (col, row),
+                    two: (col-1, row+1),
+                    three: (col-2, row+2),
+                    four: (col-3, row+3),
+                });
+                return Some(Some(Some(game_result.player)));
+            }
+        }
         None
     }
 
     pub fn get_win_constellation(&self) -> Option<WinConstellation>{
          return self.win_constellation
     }
+
+    fn add_coin_to_stack(&mut self, stack: usize) {
+        let stack_to_manipulate = &mut self.layout[stack];
+        for index in 0..stack_to_manipulate.len(){
+            let look_ahead_index = index + 1;
+            if look_ahead_index < stack_to_manipulate.len() {
+               if stack_to_manipulate[look_ahead_index] != 0 {
+                stack_to_manipulate[index] = self.current_player;
+                break;
+               }
+            } else{
+                stack_to_manipulate[index] = self.current_player;
+                break;
+            }
+        }
+    }
+
+    fn switch_player(&mut self) {
+        if self.current_player == 1 {
+            self.current_player = 2
+        } else{
+            self.current_player = 1
+        }
+    }
+}
+
+fn compute_score(game_result: &mut GameResult, field: &i8) {
+    if same_player(&*game_result, *field) {
+        game_result.field_count += 1;
+    }else{
+        game_result.player = *field;
+        game_result.field_count = 1;
+    }
+}
+
+fn same_player(game_result: &GameResult, field: i8) -> bool {
+    game_result.player == field
+}
+
+fn game_won(game_result: &GameResult) -> bool {
+    game_result.player != 0 && game_result.field_count >= 4
 }
