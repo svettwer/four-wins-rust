1735b2e723f1dd726ee7e9a1cdd641ebbe0d67a1
Sven Hettwer <Sven.Hettwer@web.de>
2022-01-29 17:34:39 +0100

    applied audit

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,10 +1,9 @@
 module.exports = {
-  purge: [
+  content: [
     './src/**/*.html',
     './src/**/*.js',
     './src/**/*.svelte',
   ],
-  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
