d80434edeffa905fbee5cd48d545462688f5dd71
Sven Hettwer <Sven.Hettwer@web.de>
2022-01-29 18:28:29 +0100

    Added basic lobby joining in UI

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,7 +1,14 @@
 <script>
     import "tailwindcss/tailwind.css"
     import FourWins from "./FourWins.svelte";
+    import Lobby from "./Lobby.svelte";
     import SetupGame from "./SetupGame.svelte";
+
+    const params = new Proxy(new URLSearchParams(window.location.search), {
+        get: (searchParams, prop) => searchParams.get(prop)
+    });
+
+    let lobby = params.lobby;
 
     let gameMode = "singleplayer" //toggle to deactivate game mode dialog
 
@@ -19,8 +26,14 @@
             <div class="text-center">
                 <div class="text-4xl text-white">Four Wins</div>
             </div>
-            <SetupGame onSetGameMode="{setGameMode}"/>
+            {#if lobby}
+                <div class="container mx-auto flex flex-col items-center justify-center space-y-4 text-white">
+                    <div class="text-2xl py-4">Multiplayer</div>
+                    <Lobby lobbyData={{lobbyId: lobby, users: []}}/>
+                </div>
+            {:else}
+                <SetupGame onSetGameMode="{setGameMode}"/>
+            {/if}
         </div>
     {/if}
-    
 </main>
